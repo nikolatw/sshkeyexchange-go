8264c413c37656f4d3edb33b86407c96048731d0
nikolatw <taskwirers@proton.me>
2022-07-31 14:15:44 +0200

    fix: renamer

--- scripts/renamer/main.go
+++ scripts/renamer/main.go
@@ -4,12 +4,17 @@ import (
 	"io/fs"
 	"io/ioutil"
 	"os"
+	"path"
 	"strings"
 )
 
 func main() {
 	binariesFolders, err := os.ReadDir("keyexchange")
 	handleError(err)
+
+	err = os.Mkdir("keyexchange/bin", 0o777)
+	handleError(err)
+
 	for _, maybeBinaryFolder := range binariesFolders {
 		if maybeBinaryFolder.Type() == fs.ModeDir {
 			binaryFolderName := "keyexchange/" + maybeBinaryFolder.Name()
@@ -19,7 +24,9 @@ func main() {
 			bin, err := ioutil.ReadFile(binaryFolderName + "/" + binaryFolder[0].Name())
 			handleError(err)
 
-			err = ioutil.WriteFile("keyexchange/"+strings.TrimPrefix(maybeBinaryFolder.Name(), "sshkeyexchange-go_"), bin, 0644)
+			extension := path.Ext(binaryFolder[0].Name())
+
+			err = ioutil.WriteFile("keyexchange/bin/"+strings.TrimPrefix(maybeBinaryFolder.Name(), "sshkeyexchange-go_")+extension, bin, 0644)
 			handleError(err)
 		}
 	}
